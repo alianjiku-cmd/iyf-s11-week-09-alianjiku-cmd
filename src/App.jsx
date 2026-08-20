aee2dfb65a2087206885e4fbaa4d0b9d622e44ad
alianjiku-cmd <alianjiku@gmail.com>
2026-08-21 00:47:23 +0300

    Save local changes before merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Layout/Header";
-import Footer from "./components/Layout/Footer";
-import Sidebar from "./components/Layout/Sidebar";
-import Home from "./pages/Home";
-import Posts from "./pages/Posts";
-import PostDetail from "./pages/PostDetail";
-import CreatePost from "./pages/CreatePost";
-import About from "./pages/About";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-
-      <div className="flex">
-        {/* ✅ Sidebar on the left */}
-        <Sidebar />
-
-        {/* ✅ Main content on the right */}
-        <main className="flex-1 p-6">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/posts/:postId" element={<PostDetail />} />
-            <Route path="/create" element={<CreatePost />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </main>
-      </div>
-
-      <Footer />
-    </BrowserRouter>
+    <div className="text-3xl font-bold underline text-blue-600">
+      Hello Tailwind!
+    </div>
   );
 }
 
-export default App;
--- src/components/Post.jsx
+++
@@ -1,9 +0,0 @@
-import PostList from "../components/PostList";
-export default function Posts() {
-  return (
-    <div>
-      <h2>Posts</h2>
-      <PostList />
-    </div>
-  );
-}
